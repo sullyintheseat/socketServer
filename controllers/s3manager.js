647f123ff3fe4c651f28401bdc2e5d5e6c6d17b6
zirra <msullivan@heysulli.com>
2019-08-14 09:24:38 -0500

    testing

--- controllers/s3manager.js
+++ controllers/s3manager.js
@@ -31,7 +31,7 @@ const S3ManagerController = {
 
   uploadFile: async (req, res) => {
     let { Bucket, Key, Body} = req.body
-    console.log(body)
+    console.log(Body)
     var uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(
       {
         Bucket, 
