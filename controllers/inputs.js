e679d7e1d6f6ee1a97fedd8900be1fc0803e156f
zirra <msullivan@heysulli.com>
2020-01-11 19:33:11 -0600

    add metrics hook

--- controllers/inputs.js
+++ controllers/inputs.js
@@ -23,11 +23,20 @@ const InputController = {
     } else {
       res.status(200).send({isActive: false})
     }
+  },
+
+  metrics: async (req, res) => {
+    try {
+      res.status(200).send('ok')
+    } catch (err) {
+      res.status(500).send(err)
+    }
   }
 }
 
 module.exports.Controller = InputController;
 module.exports.controller = (app) => {
   app.post('/v1/survey', InputController.addEntry)
+  app.post('/v1/metrics', InputController,metrics)
   app.get('/v1/survey/status/:value', InputController.surveyActive)
 }
