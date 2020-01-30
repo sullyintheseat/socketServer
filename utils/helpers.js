0ede3ee344e663b9d76e5a9da422b5b80a7cc0d0
zirra <msullivan@heysulli.com>
2020-01-29 21:01:04 -0600

    updating date lookuop

--- utils/helpers.js
+++ utils/helpers.js
@@ -31,7 +31,7 @@ const helpers = {
   }, 
 
   Today() {
-    return moment().subtract(process.env.UTC_OFFSET).format('YYYY-MM-DD')
+    return moment().add(process.env.UTC_OFFSET).format('YYYY-MM-DD')
   }
 }
 
